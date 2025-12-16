f3cee42e80a9b7cb58164d7e9f92441feede6b17
JonatasTavares1 <jonatastavares2015@gmail.com>
2025-12-16 17:06:12 -0300

    pronto

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///./repositorio_cm.db"
+    database_url: str = "postgresql://painel_projetos_especiais_user:MewImBnoF5r0Y3GmWHt63qIEJF9K2Ezs@dpg-d2mqg41r0fns73bh51m0-a.oregon-postgres.render.com/painel_projetos_especiais"
     cors_origins: str = "http://localhost:5173,http://127.0.0.1:5173"
 
     class Config:
